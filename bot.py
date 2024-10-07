6716321078d8369654076a28704cb84f550d1f12
Amin.MasterkinG <lichwow_masterking@yahoo.com>
2024-10-07 18:33:38 +0330

    Use extract_tg_query_from_url instead of getTGWebQuery

--- bot.py
+++ bot.py
@@ -12,8 +12,8 @@ import asyncio
 from pathlib import Path
 import threading
 import hashlib
-import requests
 
+from mcf_utils.utils import extract_tg_query_from_url
 import utilities.utilities as utilities
 from FarmBot.FarmBot import FarmBot
 
@@ -132,7 +132,7 @@ async def process_pg_account(account, bot_globals, log, group_id=None):
             )
             return
 
-        web_app_query = tg.getTGWebQuery(web_app_data)
+        web_app_query = extract_tg_query_from_url(web_app_data)
         if not web_app_query:
             log.error(
                 f"<r>└─ ❌ Account <c>{account['session_name']}</c> from group <c>{group_id}</c> WebApp query is not valid!</r>"
@@ -191,8 +191,7 @@ async def process_module_account(account, bot_globals, log, group_id=None):
             )
             return
 
-        tg_tmp = tgAccount()
-        web_app_query = tg_tmp.getTGWebQuery(web_app_data)
+        web_app_query = extract_tg_query_from_url(web_app_data)
         if not web_app_query:
             log.error(
                 f"<r>❌ Account {account_name} from group <c>{group_id}</c> WebApp query is not valid!</r>"
